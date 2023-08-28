bcc4bede632260b40426ecfd358fff69cdbeac45
KevinMCJ <kevin72309@gmail.com>
2023-08-27 21:44:27 -0300

    style: mediaqueries

--- client/src/components/SortBy/SortBy.jsx
+++ client/src/components/SortBy/SortBy.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
-import { sortBreedsByWeight, sortBreedsByName } from "../../redux/actions"
-import style from "./SortBy.module.css"
+import { sortBreedsByWeight, sortBreedsByName } from "../../redux/actions";
+import style from "./SortBy.module.css";
 
 const SortBy = ({ setCurrentPage }) => {
   const dispatch = useDispatch();
@@ -19,14 +19,24 @@ const SortBy = ({ setCurrentPage }) => {
     <div className={style.sort_container}>
       <div>
         <label htmlFor="sort-weight">Sort by Weight</label>
-        <select name="sort-weight" id="sort-weight" onChange={handleSelect} className={style.select}>
+        <select
+          name="sort-weight"
+          id="sort-weight"
+          onChange={handleSelect}
+          className={style.select}
+        >
           <option value="highest">Weight (Highest)</option>
           <option value="lowest">Weight (Lowest)</option>
         </select>
       </div>
       <div>
         <label htmlFor="sort-name">Sort by Name</label>
-        <select name="sort-name" id="sort-name" onChange={handleSelect} className={style.select}>
+        <select
+          name="sort-name"
+          id="sort-name"
+          onChange={handleSelect}
+          className={style.select}
+        >
           <option value="asc">Name (a-z)</option>
           <option value="desc">Name (z-a)</option>
         </select>
