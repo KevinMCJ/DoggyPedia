f29f9432fa199ec54b67bdba1393b406a7ffda11
KevinMCJ <kevin72309@gmail.com>
2023-08-28 11:08:52 -0300

    refactor: temperament controller performance.

--- api/src/controllers/temperamentsController.js
+++ api/src/controllers/temperamentsController.js
@@ -6,27 +6,38 @@ const { Temperament } = require("../db");
 const endpoint = "https://api.thedogapi.com/v1/breeds?api_key=" + API_KEY;
 
 const syncTemperaments = async () => {
-  const { data } = await axios.get(endpoint);
-  const allTemps = [];
-  let dogTemps;
+  const count = await Temperament.count();
+  let allTemps = [];
 
-  data.forEach((dog) => {
-    if (dog.temperament) {
-      dogTemps = dog.temperament.split(", ");
+  // ? Si hay temperamentos en la BD los obtiene de ahi, si no, usa la API y los carga en la BD.
+  if (count) {
+    const temperaments = await Temperament.findAll();
+    allTemps = temperaments.map(temp => temp.name);
+  } else {
+    const { data } = await axios.get(endpoint);
+    let dogTemps;
 
-      for (let temp of dogTemps) {
-        // * Para no guardar repetidos
-        if (!allTemps.includes(temp)) allTemps.push(temp);
+    if(!data) throw new Error("Couldn't get temperament data from API.");
+
+    data.forEach((dog) => {
+      if (dog.temperament) {
+        dogTemps = dog.temperament.split(", ");
+
+        for (let temp of dogTemps) {
+          // * Para no guardar repetidos
+          if (!allTemps.includes(temp)) allTemps.push(temp);
+        }
       }
-    }
-  });
-
-  // * Para cargar la BDD solo una vez.
-  !allTemps.length && allTemps.forEach(async (temp) => {
-    await Temperament.create({
-      name: temp,
     });
-  });
+
+    allTemps.sort(); // * Ordenar los temperamentos alfabéticamente
+
+    allTemps.forEach(async (temp) => {
+      await Temperament.create({
+        name: temp,
+      });
+    });
+  }
 
   return allTemps;
 };
