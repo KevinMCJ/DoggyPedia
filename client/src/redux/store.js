1839de739a22e5e1ffa6d1b0b5a816c48119c813
KevinMCJ <kevin72309@gmail.com>
2023-09-02 16:34:26 -0300

    chore: commented redux developer tools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducer";
 import thunkMiddleware from "redux-thunk";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// ? Uncomment this to use redux developer tools and import => compose from "redux"
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancer(applyMiddleware(thunkMiddleware))
+  applyMiddleware(thunkMiddleware)
 );
+
+// ? Change the second arg of createStore to this to work with redux developer tools
+// composeEnhancer(applyMiddleware(thunkMiddleware))
 
 export default store;
