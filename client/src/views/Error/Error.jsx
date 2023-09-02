8365041bd57bcce995c2d7358c1689a14b37a142
KevinMCJ <kevin72309@gmail.com>
2023-09-02 13:28:36 -0300

    feat: 404 view.

--- client/src/views/Error/Error.jsx
+++ client/src/views/Error/Error.jsx
@@ -1,8 +1,24 @@
-const Error = () => {
+import { useEffect } from "react";
+import style from "./Error.module.css";
+import { Link } from "react-router-dom";
+
+const Error = ({ setShowNavBar }) => {
+  
+  useEffect(() => {
+    setShowNavBar(false);
+
+    return () => {
+      setShowNavBar(true);
+    };
+  }, [setShowNavBar]);
+
   return (
-    <>
-      <h1>ERROR 404 ESTA PAGINA NO EXISTE</h1>
-    </>
+    <section className={style.container}>
+      <div className={style.content_box}>
+        <h1 className={style.title}>Page not found</h1>
+        <Link to="/home" className={style.backBtn}>Go back to home</Link>
+      </div>
+    </section>
   );
 };
 
